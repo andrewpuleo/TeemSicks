7d742f3de98e41fab409fbf5b84590438ad8835e
James Asbury <jamesasbury03@gmail.com>
2018-10-14 15:17:46 -0700

    Made a pretty navbar with icons for login and cart

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,40 +1,58 @@
 <template>
   <div id="app">
-    <div id="logo" class="logo">
-      <img align="left" src="./assets/foxycle.png">
+    <div id="logo">
+      <router-link to="/"><img align="left" src="./assets/foxycle.png"></router-link>
    </div>
-   <nav class="navbar navbar-expand-md navbar-light bg-light" id="nav">
-      <button class="navbar-toggler navbar-toggle btn" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-         <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-               <router-link to="/">Home</router-link> |
-            </li>
-            <li class="nav-item active">
-               <router-link to="/store">Store</router-link> |
-            </li>
-            <li class="nav-item active">
-               <router-link to="/contact">Contact</router-link> |
-            </li>
-            <li class="nav-item active">
-               <router-link to="/login">Login</router-link>
-            </li>
-         </ul>
+   <nav class="navbar navbar-inverse navbar-expand-sm navbar-light bg-light">
+      <div class="container">
+         <button class="navbar-toggler navbar-toggle btn" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <!-- Left items: Home, Store, Contact -->
+            <ul class="navbar-nav mr-auto" id="navleft">
+               <li class="nav-item active">
+                  <router-link to="/">Home</router-link> |
+               </li>
+               <li class="nav-item active">
+                  <router-link to="/store">Store</router-link> |
+               </li>
+               <li class="nav-item active">
+                  <router-link to="/contact">Contact</router-link>
+               </li>
+            </ul>
+
+            <!-- Right items: Login and cart -->
+            <ul class="navbar-nav navbar-right" id="navright">
+               <li class="nav-item active">
+                  <router-link to="/login"><img src="./assets/user.png"> Login</router-link>
+               </li>
+               <li class="nav-item active">
+                  <router-link to="/cart"><img src="./assets/cart.png">Cart</router-link>
+               </li>
+            </ul>
+         </div>
       </div>
    </nav>
-
-    <!-- div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/store">Store</router-link> |
-      <router-link to="/contact">Contact</router-link> |
-      <router-link to="/login">Login</router-link>
-   </div -->
     <router-view/>
 </div>
 
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue';
+import index from '@/components/index.vue'; // @ is an alias to /src
+//import Navbar from '@/components/Navbar.vue';
+
+@Component({
+  components: {
+  HelloWorld, index,
+  },
+  })
+export default class Home extends Vue {}
+</script>
 
 <style lang="scss">
 #app {
@@ -44,11 +62,12 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 1px;
+#navleft {
+  padding: 4px;
   a {
     padding-left: 10px;
     padding-right: 10px;
+    padding-top: 1rem;
     font-weight: bold;
     color: #2c3e50;
     &.router-link-exact-active {
@@ -56,11 +75,28 @@
     }
   }
 }
+#navright {
+  padding: 0px;
+  a {
+    padding-left: 0px;
+    padding-right: 0px;
+    padding-top: 1rem;
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #ff6600;
+    }
+  }
+  img{
+     height: 1rem;
+     padding-left: 2rem;
+     height: 1.5rem;
+     text-align: right;
+ }
+}
 #logo {
    height: 6rem;
    padding-left: 2rem;
-   padding-top: 1rem;
-   padding-bottom: 1rem;
     img{
       height: 6rem;
       text-align: left;
---
+++ app/src/views/Cart.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="cart">
+    <HelloWorld msg="Welcome to your cart"/>
+    <index/>
+    <div class="container">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue';
+import index from '@/components/index.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+  HelloWorld, index,
+  },
+  })
+export default class Cart extends Vue {}
+</script>
