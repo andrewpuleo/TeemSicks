90a01aaa1505e1eb6aa80150834ed17cb6e756ea
seanq101 <seanq101@gmail.com>
2018-10-18 18:53:44 -0700

    Sean's broken slider, about to be deleted

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,14 +1,16 @@
 import Vue from 'vue';
-import BootstrapVue from 'bootstrap-vue';
+//import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './../node_modules/bootstrap/dist/css/bootstrap.css';
 import './../node_modules/jquery/dist/jquery.min.js';
 import './../node_modules/bootstrap/js/dist/button.js';
+import './../node_modules/slick-carousel/slick/slick.js';
+import './../node_modules/slick-carousel/slick/slick.css';
 
 Vue.config.productionTip = false;
-Vue.use(BootstrapVue);
+//Vue.use(BootstrapVue);
 
 new Vue({
   router,
