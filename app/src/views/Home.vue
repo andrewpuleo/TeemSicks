f1d21d3ec8904e30f35fe532f06f8eed7f4fa819
seanq101 <seanq101@gmail.com>
2018-10-22 11:45:52 -0700

    Working on the Store

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -7,6 +7,7 @@
     <div class="home">
       <h1>Welcome to your new home</h1>
     </div>
+    
     <HomeSlider/>
 
   </div>
--- app/src/views/Store.vue
+++ app/src/views/Store.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="store">
+  <div class="store container">
     <h1>Welcome to the Store</h1>
+    <div class="row">
+      <div class="col-sm-2">
+      yeet
+      </div>
+      <div class="col-sm-10 store_container">
+        
+          <StoreItem/>
+        
+          <StoreItem/>
+        
+          <StoreItem/>
+
+          <StoreItem/>
+
+          <StoreItem/>
+
+          <StoreItem/>
+
+          <StoreItem/>
+        
+       
+      
+      
+      </div>
+    </div>
   </div>
+  
 </template>
+
+<script>
+import  StoreItem  from "@/components/storeItem.vue";
+export default {
+  components: {StoreItem},
+  
+}
+</script>
+
+
+<style scoped>
+.store_container {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+</style>
