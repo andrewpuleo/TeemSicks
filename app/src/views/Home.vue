ec71f77c8753c7ec2985a1db41309207e93f4bd2
seanq101 <seanq101@gmail.com>
2018-10-23 20:05:48 -0700

    Home.vue should no longer conflict

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -7,17 +7,73 @@
     <div class="home">
       <h1>Welcome to your new home</h1>
     </div>
-    
     <HomeSlider/>
+    <div class="header">
+     <h2>About Foxycle</h2>
+   </div>
 
-  </div>
+   <div class="row">
+     <div id="newcolumn" style="background-color:#FFFDF4;">Who We Are</div>
+     <div id="newcolumn" style="background-color:#FFFDF4;">Why We Are</div>
+     <div id="newcolumn" style="background-color:#FFFDF4">How We Are</div>
+   </div>
+
+</div>
+    
+    
+
 </template>
 
 <script>
 import  HomeSlider  from "@/components/homeSlider";
 export default {
   components: {HomeSlider},
-  
-}
+  }
 </script>
-<!-- carousel-1.vue -->
+
+<style lang="scss">
+* {
+    box-sizing: border-box;
+}
+
+body{
+   background-color: #FFFDF4 !important;
+}
+
+.header {
+    background-color: #ddd;
+    font-weight: bold;
+    background-size: cover;
+    color: rgb(241, 63, 9);
+    padding: 150px;
+    text-align: center;
+    font-size: 35pt;
+    font-family:inherit;
+    font-style:oblique;
+    height: 400px;
+}
+
+#newcolumn{
+    border: 30px;
+    font: inherit;
+    color: rgb(241, 63, 9);
+    float:left;
+    width: 33.33%;
+    padding: 110px;
+    text-align: center;
+    height: 500px;
+    padding-bottom: 10rem;
+}
+
+.row:after {
+    content: "";
+    display: table;
+    clear: both;
+
+}
+
+@media (max-width: 600px) {
+    .column {
+        width: 100%;
+    }
+}
