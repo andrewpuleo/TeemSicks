3bb3b89286067308059b14985f1dbae7dda672b9
mariowuchen <mario.wuchen@gmail.com>
2018-11-01 00:17:00 -0700

    added cart

--- app/src/main.ts
+++ app/src/main.ts
@@ -6,7 +6,6 @@ import store from './store';
 import './../node_modules/bootstrap/dist/css/bootstrap.css';
 import VModal from 'vue-js-modal';
 
-
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(VModal);
