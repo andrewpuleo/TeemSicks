7a983aa46f9f9b09db43f96d39b7e6daf84a0f59
Andrew Puleo <apuleo36@gmail.com>
2018-11-01 14:22:04 -0700

    clean out files involving todos

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const todosRouter = require('./routes/todo');
 
 const app = express();
 
@@ -22,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/todos', todosRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
