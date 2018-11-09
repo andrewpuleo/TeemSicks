4f0ed999c895d168acc6806a80d4bcde1cc181b3
James Asbury <jamesasbury03@gmail.com>
2018-11-08 20:49:47 -0800

    The item links work!

---
+++ app/src/components/item.vue
@@ -0,0 +1,10 @@
+<template>
+      <div>Some text to search for  {{id}}</div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop} from 'vue-property-decorator';
+@Component
+export default class Item extends Vue {
+   @Prop() id!: Number;
+}
+</script>
--- app/src/router.ts
+++ app/src/router.ts
@@ -78,5 +78,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/confirmation.vue'),
     },
+    {
+      path: '/item/:id',
+      name: "item",
+      props:true,
+      component: () => import(/* webpackChunkName: "about" */ './components/item.vue'),
+   },
   ],
 });
