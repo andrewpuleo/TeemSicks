41394c972c01afc1f77facdd097223397ef81c2b
lacraft <lacraft@calpoly.edu>
2018-11-23 21:14:34 -0800

    Update account.vue

--- app/src/views/account.vue
+++ app/src/views/account.vue
@@ -1,9 +1,20 @@
 <template>
     <div id="account">
-        <h1>Secure Area</h1>
-        <p>
-            This is a secure area
-        </p>
+        <div class="container-fluid headerimg">
+            <div class="row">
+                <img src="../assets/AccountBike.png">
+                <div class="centered">
+                    <h> My Account </h>
+                </div>
+            </div>
+            <div class="row">
+                <div v-if=this.$store.getters.getIsLoggedIn>
+
+                    <AccountInfo></AccountInfo>
+                </div>
+                <h1 v-else>Nope</h1>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -23,4 +34,40 @@
         padding: 20px;
         margin-top: 10px;
     }
+
+    .headerimg img{
+        height: 600px;
+        width: 100%;
+    }
+
+
+    .centered{
+        position: absolute;
+        top: 26rem;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: white;
+        font-size: 5rem;
+    }
 </style>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import axios from 'axios';
+import ToDo from '@/components/ToDo.vue';
+import { User } from '@/models';
+import  AccountInfo from "@/components/accountInfo.vue";
+
+@Component({ components: { AccountInfo, } })
+export default class Account extends Vue {
+  visitor: User[] = [];
+  mounted() {
+
+    axios.get('/api/Users')
+      .then((response) => {
+        this.visitor = response.data.user;
+        console.log(this.visitor);
+      });
+  }
+}
+</script>
