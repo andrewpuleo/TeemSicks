8f01b01c4cc42677b26cca0c40d68c926d6760a8
lacraft <lacraft@calpoly.edu>
2018-11-27 18:36:36 -0800

    displaying account info

--- app/src/views/account.vue
+++ app/src/views/account.vue
@@ -1,20 +1,23 @@
 <template>
     <div id="account">
-        <h1>Secure Area</h1>
-        <p>
-            This is a secure area
-        </p>
+        <div class="container-fluid headerimg">
+            <div class="row">
+                <img src="../assets/AccountBike.png">
+                <div class="centered"> My Account
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div v-if="this.$store.getters.getIsLoggedIn">
+                        <AccountInfo v-bind:visitor="visitor"/>
+                    </div>
+                    <h1 v-else>Nope</h1>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
-<script>
-    export default {
-        name: 'account',
-        data() {
-            return {};
-        }
-    }
-</script>
 
 <style scoped>
     #secure {
@@ -23,4 +26,40 @@
         padding: 20px;
         margin-top: 10px;
     }
+
+    .headerimg img{
+        height: 600px;
+        width: 100%;
+    }
+
+
+    .centered{
+        position: absolute;
+        top: 26rem;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: white;
+        font-size: 5rem;
+    }
 </style>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import axios from 'axios';
+import ToDo from '@/components/ToDo.vue';
+import { User } from '@/models';
+import  AccountInfo from "@/components/accountInfo.vue";
+
+@Component({ components: { AccountInfo, } })
+export default class Account extends Vue {
+  visitor: User = new User();
+  mounted() {
+    if(this.$store.getters.getIsLoggedIn){
+        axios.get(`/api/users/${this.$store.getters.getUID}`)
+        .then((response) => {
+            this.visitor = response.data.user;
+        });
+    }
+  }
+}
+</script>
