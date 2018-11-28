1fd32c390811ef3f1bbc024b52da6d2bd64d6b6a
Andrew Puleo <apuleo36@gmail.com>
2018-11-27 20:05:51 -0800

    add config to run tests

--- server/tests/truncate.js
+++ server/tests/truncate.js
@@ -2,6 +2,8 @@ const { User, Session, ToDo } = require('../models');
 
 const models = [ToDo, Session, User];
 
+// Prof Fox says this will need to be edited due to associations
+// that is removing them in a certain order
 module.exports = function truncate() {
   return Promise.all(
     models.map(model =>
