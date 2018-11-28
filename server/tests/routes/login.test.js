bcee90ccf9bb6d70831d01478e29cffa372844c6
Andrew Puleo <apuleo36@gmail.com>
2018-11-27 19:39:54 -0800

    add file from project shell

---
+++ server/tests/routes/login.test.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const { User } = require('../../models');
+const app = require('../../app');
+const truncate = require('../truncate');
+
+describe('login', () => {
+  beforeAll(() => truncate());
+  afterAll(() => User.Sequelize.close());
+  it('should give back a user_id for a correct login', done => {
+    User.create({ username: `test${new Date().getTime()}`, password: 'test' }).then(user =>
+      request(app)
+        .post('/login')
+        .send({ username: user.username, password: user.password })
+        .then(res => {
+          expect(res.body).toEqual({ user_id: user.id });
+          done();
+        })
+    );
+  });
+  it('should give back a error for login', done => {
+    User.create({ username: `test${new Date().getTime()}`, password: 'test' }).then(user =>
+      request(app)
+        .post('/login')
+        .send({ username: user.username, password: 'not correct' })
+        .then(res => {
+          expect(res.body).toEqual({ error: 'incorrect username or password' });
+          done();
+        })
+    );
+  });
+});
---
+++ server/tests/truncate.js
@@ -0,0 +1,14 @@
+const { User, Session, ToDo } = require('../models');
+
+const models = [ToDo, Session, User];
+
+module.exports = function truncate() {
+  return Promise.all(
+    models.map(model =>
+      model.destroy({
+        where: {},
+        force: true
+      })
+    )
+  );
+};
