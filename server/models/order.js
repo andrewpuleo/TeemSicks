fe738985b2b881bc9d718aecf15f6d8bd87180bb
Andrew Puleo <apuleo36@gmail.com>
2018-11-30 13:32:40 -0800

    resolve merge conflicts

--- server/models/order.js
+++ server/models/order.js
@@ -8,13 +8,8 @@ module.exports = (sequelize, DataTypes) => {
   Order.associate = function(models) {
     // associations can be defined here
     Order.belongsTo(models.User);
-<<<<<<< HEAD
     Order.belongsTo(models.Address);
     Order.belongsTo(models.OrderItem);
-=======
-    Order.hasOne(models.Address);
-    Order.hasMany(models.OrderItem);
->>>>>>> 5c61d6af0b2bedf235c6d27125a04777e72a1b3d
   };
 
   return Order;
