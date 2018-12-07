6abef2f83ef4b2fd876207e369625d22ffb337af
lacraft <lacraft@calpoly.edu>
2018-12-07 11:50:58 -0800

    order details show

--- app/src/components/ordersChart.vue
+++ app/src/components/ordersChart.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="container">
+        <OrderInfo
+        v-if="viewOrderInfo"
+        v-bind:currentOrderId="this.currentOrderId"
+        v-bind:currentProducts="this.currentProducts"
+        v-on:close="viewOrderInfo = false"/>
         <table class="table table-striped">
             <thead>
                 <tr>
                 <th scope="col">ID</th>
                 <th scope="col">UserID</th>
                 <th scope="col">AddressID</th>
+                <th scope="col">Details</th>
+                <th scope="col">Finish</th>
                 </tr>
             </thead>
             <tbody>
@@ -14,6 +21,8 @@
                     <td>{{order.id}}</td>
                     <td>{{order.userId}}</td>
                     <td>{{order.addressId}}</td>
+                    <td><button v-on:click="getOrderInfo(), viewOrderInfoModal(order.id)"> More Info </button> </td>
+                    <td><button v-on:click="fufillOrder(order)"> Fufill Order </button></td>
 
                 </tr>
 
@@ -23,24 +32,76 @@
 </template>
 
 
-
 <script lang="ts">
  import { order } from '@/models';
  import { product } from '@/models';
  import axios from 'axios';
  import { Component, Prop, Vue } from 'vue-property-decorator';
- @Component
+import { orderItem } from '../models/orderItem';
+import OrderInfo from '@/components/orderInfo.vue';
+
+@Component({components: {OrderInfo}})
     export default class OrdersChart extends Vue{
         orders: order[] = [];
+        orderItems: orderItem[] = [];
+        products: product[] = [];
+        orderId: number = null;
+        viewOrderInfo = false;
+        currentOrderId: number = null;
+        currentProducts: product[] = [];
+        orderFulls = [];
 
         mounted(){
-            axios.get('/api/Orders')
+            axios.get('/api/orders')
             .then((response) => {
               console.log(response)
                 this.orders = response.data.orders;
-                console.log(this.orders);
+                console.log("orders" , this.orders);
             });
-
+            axios.get(`/api/orderitem`)
+            .then((response) => {
+                this.orderItems = response.data.orderitems;
+                console.log("order items", this.orderItems);
+            });
+            console.log("our order", this.orderItems);
+            axios.get(`/api/products`)
+            .then((response) => {
+                this.products = response.data.products;
+            });
         };
+
+        fufillOrder(order){
+            
+
+        }
+
+        getOrderInfo(){
+            this.orderFulls = [];
+            for(let i = 0; i < this.orders.length; i++){
+                console.log(this.orders[i]);
+                var fullOrder = {
+                    orderId: this.orders[i].id,
+                    userId: this.orders[i].userId,
+                    addressID: this.orders[i].addressId,
+                    products: this.orderItems.filter(orderItem => {
+                        return orderItem.orderid === this.orders[i].id
+                    }).map(orderItem => this.products.find(product => product.id === orderItem.productId))
+                };
+                this.orderFulls.push(fullOrder);
+            }
+        }
+
+        viewOrderInfoModal(id){
+            this.currentProducts = [];
+            for(let i = 0; i < this.orderFulls.length; i++){
+                if(id === this.orderFulls[i].orderId){
+                    this.currentProducts = this.orderFulls[i].products;
+                    this.currentOrderId = id;
+                }
+            }
+            this.viewOrderInfo  = true
+        }
+
     };
+
 </script>
